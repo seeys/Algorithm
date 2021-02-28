83543a1837655ea53a9fd9c392f6536669c290cb
seeys <es2008e@naver.com>
2021-03-01 00:47:47 +0900

    sort

---
+++ Sort/Sort/11651.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool compare(const pair<int, int>& a, const pair<int, int>& b)
+{
+	if (a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
+}
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<pair<int, int>> p;
+	int i, j;
+	for (int k = 0; k < n; k++) {
+		cin >> i >> j;
+		p.push_back({ i, j });
+	}
+	sort(p.begin(), p.end(), compare);
+	for (auto c : p)
+		cout << c.first << " " << c.second << "\n";
+}
