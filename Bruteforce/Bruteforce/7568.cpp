210d8c50c4292c6188928767bf1ad08e6680ddd4
seeys <es2008e@naver.com>
2021-04-08 17:52:13 +0900

    0408

--- Bruteforce/Bruteforce/7568.cpp
+++ Bruteforce/Bruteforce/7568.cpp
@@ -1,11 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+vector<pair<int, int>> vp;
+int res[51];
+void func(int n)
+{
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i != j)
+			{
+				if (vp[i].first < vp[j].first && vp[i].second < vp[j].second)
+					res[i]++;
+				else
+					continue;
+			}
+			else
+				continue;
+		}
+	}
+	for (int k = 0; k < n; k++)
+	{
+		cout << res[k] + 1 << " ";
+	}
+}
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int num;
+	int num, x, y;
 	cin >> num;
 	
+	for (int i = 0; i < num; i++)
+	{
+		cin >> x >> y;
+		vp.push_back({ x, y });
+	}
+
+	func(num);
+	return 0;
 }
