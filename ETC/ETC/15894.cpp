952c1fd9d36c404a32e0a0ce0eb6a1007e722e89
seeys <es2008e@naver.com>
2021-05-11 19:50:00 +0900

    create

---
+++ ETC/ETC/15894.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	unsigned int k;
+	unsigned int re;
+	cin >> k;
+	re = k * 4;
+	cout << re;
+}
---
+++ ETC/ETC/5522.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int k, sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> k;
+		sum += k;
+	}
+	cout << sum;
+}
