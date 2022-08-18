fbdb4e83abb97522549c138a3471f3410973ccd2
seni <69909093+seeys@users.noreply.github.com>
2022-08-18 23:02:53 +0900

    Add files via upload

---
+++ Silver/1074.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(int n, int r, int c){
+    if(n == 0) return 0;
+    int half = 1 << (n-1);
+    if(r < half && c < half) return func(n-1,r,c);
+    if(r < half && c >= half) return half*half + func(n-1,r,c-half);
+    if(r >= half && c < half) return 2*half*half + func(n-1,r-half,c);
+    return 3*half*half + func(n-1,r-half,c-half);
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N, r, c;
+    cin >> N >> r >> c;
+    cout<<func(N,r,c);
+}
